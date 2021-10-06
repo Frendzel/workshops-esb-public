8ddbeb543a8136530f94b2ffa66b9daa3f4537a6
Marcin Erbel <marcinerbel@protonmail.com>
2021-10-06 15:41:33 +0200

    3 day workshops * BookingRestService implementation * Add rest adapter to features file

--- booking-parent/booking-adapter-rest/src/main/java/pl/vavatech/BookingRestService.java
+++ booking-parent/booking-adapter-rest/src/main/java/pl/vavatech/BookingRestService.java
@@ -1,4 +1,22 @@
 package pl.vavatech;
 
+
+import pl.vavatech.commons.Booking;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+
+@Path("bookingservice")
 public interface BookingRestService {
+
+    @POST
+    @Path("booking")
+    @Consumes({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
+    @Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
+    Response save(Booking booking);
 }
